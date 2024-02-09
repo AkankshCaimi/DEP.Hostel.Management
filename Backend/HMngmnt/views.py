abd5aff6648e03b7b3c3bb2b4e12b64b74646c66
TheProBro <devanshu.dhawan18@gmail.com>
2024-02-09 22:56:37 +0530

    first.2

--- Backend/HMngmnt/views.py
+++ Backend/HMngmnt/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+
+@csrf_exempt
 def index(request):
     return HttpResponse("Hello, world. You're at the HMngmnt index.")
+
+@csrf_exempt
+def home(request):
+    return HttpResponse("this is home")
