1f6d90bedbc6361afc5329ea895488b312733407
Abhishek <jaiswalabhi0786@gmail.com>
2024-02-12 18:23:43 +0530

    yp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Signup from './screens/Signup';
 import Form from './screens/Form';
 import Headers from './components/Headers';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import StudentSignup from './screens/StudentSignup';
+import FacultySignup from './screens/FacultySignup';
+import AdminSignup from './screens/AdminSignup';
 import { useState } from 'react';
 
 function App() {
@@ -16,9 +19,9 @@ function App() {
         <Route path="/" element={<Login showPopup={showPopup} setShowPopup={setShowPopup} />} />
         <Route path="/login" element={<Login showPopup={showPopup} setShowPopup={setShowPopup} />} />
         <Route path="/signup" element={<Signup showPopup={showPopup} setShowPopup={setShowPopup}/>} />
-        <Route path="/signup/student" element={<Signup type="student" />} />
-        <Route path="/signup/faculty" element={<Signup type="faculty" />} />
-        <Route path="/signup/admin" element={<Signup type="admin" />} />
+        <Route path="/signup/student" element={<StudentSignup />} />
+        <Route path="/signup/faculty" element={<FacultySignup/>} />
+        <Route path="/signup/admin" element={<AdminSignup/>} />
         <Route path='/test' element={<Form />} />
       </Routes>
     </>
